199db0f1f46416872718e5de67d4e7981fd81c16
abylay <a.amirov2000@gmail.com>
2023-10-02 21:33:36 +0600

    test

--- js/cafe.js
+++ js/cafe.js
@@ -128,40 +128,11 @@ var Cafe = {
         Cafe.updateItem(itemEl, delta);
     },
     formatPrice: function (price) {
-        return "$" + Cafe.formatNumber(price / 1000, 2, ".", ",");
+        return Cafe.formatNumber(price) + " ₸";
     },
-    formatNumber: function (number, decimals, decPoint, thousandsSep) {
+    formatNumber: function (number) {
         number = (number + "").replace(/[^0-9+\-Ee.]/g, "");
-        var n = !isFinite(+number) ? 0 : +number;
-        var prec = !isFinite(+decimals) ? 0 : Math.abs(decimals);
-        var sep = typeof thousandsSep === "undefined" ? "," : thousandsSep;
-        var dec = typeof decPoint === "undefined" ? "." : decPoint;
-        var s = "";
-        var toFixedFix = function (n, prec) {
-            if (("" + n).indexOf("e") === -1) {
-                return +(Math.round(n + "e+" + prec) + "e-" + prec);
-            } else {
-                var arr = ("" + n).split("e");
-                var sig = "";
-                if (+arr[1] + prec > 0) {
-                    sig = "+";
-                }
-                return (+(
-                    Math.round(+arr[0] + "e" + sig + (+arr[1] + prec)) +
-                    "e-" +
-                    prec
-                )).toFixed(prec);
-            }
-        };
-        s = (prec ? toFixedFix(n, prec).toString() : "" + Math.round(n)).split(".");
-        if (s[0].length > 3) {
-            s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
-        }
-        if ((s[1] || "").length < prec) {
-            s[1] = s[1] || "";
-            s[1] += new Array(prec - s[1].length + 1).join("0");
-        }
-        return s.join(dec);
+        return !isFinite(+number) ? 0 : +number;
     },
     updateMainButton: function () {
         var mainButton = Telegram.WebApp.MainButton;
